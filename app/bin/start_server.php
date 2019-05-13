2acc1fd48180582faababeb0e5d439aa568d9ee8
Vesko Kenashkov <vesko@azonmedia.com>
2019-05-13 15:37:49 +0300

    - added middleware

--- app/bin/start_server.php
+++ app/bin/start_server.php
@@ -3,6 +3,10 @@
 namespace Azonmedia\Glog;
 
 use Guzaba2\Kernel\Kernel;
+use Guzaba2\Authorization\AuthorizationMiddleware;
+use Guzaba2\Authorization\FilteringMiddleware;
+use Guzaba2\Mvc\ExecutorMiddleware;
+use Guzaba2\Mvc\RoutingMiddleware;
 
 require_once('../../vendor/autoload.php');
 
@@ -15,10 +19,17 @@ const APP_CONFIG = [
 //Kernel::run_swoole('0.0.0.0', 8081);
 
 $bootstrap = function() {
-    $request_handler = $request_handler ?? new \Guzaba2\Swoole\RequestHandler();
-    $http_server = new \Guzaba2\Swoole\Server(APP_CONFIG['swoole_host'], APP_CONFIG['swoole_port'], []);
-    $http_server->on('request', $request_handler);
-    $http_server->start();
+
+    $middlewares = [];
+    $middlewares[] = new RoutingMiddleware();
+    $middlewares[] = new FilteringMiddleware();
+    $middlewares[] = new AuthorizationMiddleware();
+    $middlewares[] = new ExecutorMiddleware();
+
+    $RequestHandler = new \Guzaba2\Swoole\RequestHandler($middlewares);
+    $HttpServer = new \Guzaba2\Swoole\Server(APP_CONFIG['swoole_host'], APP_CONFIG['swoole_port'], []);
+    $HttpServer->on('request', $RequestHandler);
+    $HttpServer->start();
 
     return self::EXIT_SUCCESS;
 };
