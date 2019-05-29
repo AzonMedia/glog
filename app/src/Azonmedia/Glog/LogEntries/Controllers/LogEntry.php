92a78f00aebbb0d6431d8f3478769b6a533940e0
Vesko Kenashkov <vesko@azonmedia.com>
2019-05-29 15:58:21 +0300

    fixes

--- app/src/Azonmedia/Glog/LogEntries/Controllers/LogEntry.php
+++ app/src/Azonmedia/Glog/LogEntries/Controllers/LogEntry.php
@@ -3,6 +3,7 @@
 namespace Azonmedia\Glog\LogEntries\Controllers;
 
 use Guzaba2\Mvc\Controller;
+use Guzaba2\Translator\Translator as t;
 use Psr\Http\Message\ResponseInterface;
 
 class LogEntry extends Controller
@@ -15,7 +16,11 @@ class LogEntry extends Controller
 
     public function create() : ResponseInterface
     {
+        $Response = parent::get_structured_ok_response();
+        $struct =& $Response->getBody()->getStructure();
+        $struct['message'] = sprintf(t::_('The log entry is accepted.'));
 
+        return $Response;
     }
 
     public function update(int $id) : ResponseInterface
--- app/src/Azonmedia/Glog/LogEntries/Views/LogEntry.php
+++ app/src/Azonmedia/Glog/LogEntries/Views/LogEntry.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Azonmedia\Glog\LogEntries\Views;
 
 class LogEntry
 {
