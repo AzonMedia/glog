a336ff8f1d74287fad98e24dddd09c3b9e440017
Vesko Kenashkov <vesko@azonmedia.com>
2019-09-09 11:11:28 +0300

    - fixed view

--- app/src/Azonmedia/Glog/LogEntries/Views/LogEntry.php
+++ app/src/Azonmedia/Glog/LogEntries/Views/LogEntry.php
@@ -2,11 +2,15 @@
 
 namespace Azonmedia\Glog\LogEntries\Views;
 
-class LogEntry
+use Guzaba2\Mvc\PhpView;
+
+class LogEntry extends PhpView
 {
     public function view()
     {
-
+        $structure = $this->get_structure();
+        print '<h1>Showing data for log entry #'.$structure['log_entry_id'].'</h1>';
+        print '<p>'.$structure['log_entry_content'].'</p>';
     }
 
     public function create()
